346e88941ad646ba74d1913ad5eecbde25962567
lenzju <lenzju@katharineum.de>
2026-05-05 13:10:52 +0200

    Create ml_model.py

---
+++ utils/ml_model.py
@@ -0,0 +1,54 @@
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+import cv2
+
+# Modell laden
+model = load_model("models/keras_Model.h5", compile=False)
+
+# Labels laden
+class_names = open("models/labels.txt", "r").readlines()
+
+
+def classify_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+
+    states = []
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_idx = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # OpenCV BGR -> RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        image = Image.fromarray(frame_rgb)
+
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+        image_array = np.asarray(image)
+
+        # WICHTIG: Teachable Machine Normalisierung
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        data[0] = normalized_image_array
+
+        prediction = model.predict(data, verbose=0)
+
+        index = np.argmax(prediction)
+        confidence = prediction[0][index]
+
+        if confidence > 0.8:
+            label = class_names[index].strip()
+            states.append((frame_idx, label))
+
+        frame_idx += 1
+
+    cap.release()
+
+    return states, fps
